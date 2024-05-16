fdf3db4cb4ca0fc49c849fe00a456d5ef7b43c69
wangd14 <wangd14@rpi.edu>
2024-05-16 01:45:41 -0700

    finished styling for matches

--- frontend/app/(tabs)/matches.tsx
+++ frontend/app/(tabs)/matches.tsx
@@ -1,27 +1,53 @@
-import { Image, StyleSheet, Platform, SafeAreaView } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  TextInput,
+  Platform,
+  SafeAreaView,
+} from "react-native";
 
 import { Text, ScrollView } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Container from "@/components/Container";
 import BobaBlock from "@/components/matches/BobaBlock";
 
 export default function MatchesScreen() {
   //use a useeffect to constantly update the bobas
   return (
-    <SafeAreaView style={styles.container}>
+    <Container>
       {}
       {/*add a bobablock per each match */}
-      <BobaBlock label="first one" />
-      <BobaBlock label="second one" />
-      <Text>Hello</Text>
-    </SafeAreaView>
+      <View style={styles.searchbar}>
+        <Ionicons name="search" size={30} color="#a38f93" />
+        <TextInput
+          placeholder="Search for Boba"
+          placeholderTextColor="#a38f93"
+        ></TextInput>
+      </View>
+      <View style={styles.separateMatches}>
+        <BobaBlock label="first one" />
+        <BobaBlock label="second one" />
+      </View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  separateMatches: {
     display: "flex",
-    alignItems: "center",
-
-    padding: 20,
+    gap: 20,
+  },
+  searchbar: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 3,
+    borderColor: "grey",
+    backgroundColor: "#f9e4e4",
+    borderRadius: 15,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    width: "90%",
+    height: 50,
   },
 });
--- frontend/components/matches/BobaBlock.tsx
+++ frontend/components/matches/BobaBlock.tsx
@@ -8,15 +8,28 @@ type BobaBlockType = {
 const BobaBlock = (props: BobaBlockType) => {
   return (
     <View style={styles.blockBorder}>
-      <Image
-        source={require("../../assets/images/DrinkSelection.png")}
-        style={styles.bobaImage}
-      />
+      <View style={styles.bobaImageFrame}>
+        <Image
+          source={require("../../assets/images/brownsugarboba.png")}
+          style={styles.bobaImage}
+        />
+      </View>
       <View style={styles.bobaDescription}>
         <Text style={styles.bobaName}>{props.label}</Text>
         <View>
-          <Image></Image>
-          <Text>Dummy Text</Text>
+          <Image
+            source={require("../../assets/images/FiveStar.png")}
+            style={styles.rating}
+          />
+          <Text style={styles.bobaInfo}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </Text>
+        </View>
+        <View style={styles.positionInfo}>
+          <View style={styles.moreInfoButton}>
+            <Text style={styles.infoButtonText}>MORE</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -25,23 +38,65 @@ const BobaBlock = (props: BobaBlockType) => {
 
 const styles = StyleSheet.create({
   blockBorder: {
-    borderColor: "black",
-    borderRadius: 6,
-    borderWidth: 2,
+    borderRadius: 15,
     display: "flex",
     gap: 10,
     flexDirection: "row",
     justifyContent: "center",
     width: "80%",
-    padding: 30,
+    padding: 10,
+    paddingLeft: 45,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  bobaImageFrame: {
+    backgroundColor: "#F8E3E5",
+    borderRadius: 15,
+    width: 100,
+    paddingTop: 10,
+    paddingBottom: 10,
+    alignItems: "center",
   },
   bobaImage: { height: 150, width: 60 },
-  bobaDescription: { width: "80%" },
+  bobaDescription: {
+    width: "80%",
+  },
   bobaName: {
     width: "100%",
     fontSize: 20,
     fontWeight: "600",
-    paddingBottom: 15,
+    paddingBottom: 12,
+    color: "#6f5c63",
+    marginTop: 10,
+  },
+  rating: { marginBottom: 4, width: 100, height: 15 },
+  bobaInfo: {
+    color: "#6f5c63",
+    fontWeight: "400",
+    fontSize: 12,
+    lineHeight: 17,
+    width: 190,
+  },
+  positionInfo: { marginTop: 15, marginLeft: 95 },
+  moreInfoButton: {
+    borderColor: "#e88a84",
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: "#e2a191",
+    padding: 8,
+    width: 80,
+    alignItems: "center",
+  },
+  infoButtonText: {
+    color: "white",
+    fontWeight: "700",
   },
 });
 
