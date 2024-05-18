93dd500247f65b16e1e425ada72eecc0bbfd7b54
wangd14 <wangd14@rpi.edu>
2024-05-18 14:32:16 -0700

    added toast messages

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,41 +1,43 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Toast from "react-native-toast-message";
 
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-	const [loaded] = useFonts({
-		OverpassExtraLight: require('../assets/fonts/Overpass-ExtraLight.ttf'),
-		OverpassLight: require('../assets/fonts/Overpass-Light.ttf'),
-		Overpass: require('../assets/fonts/Overpass-Regular.ttf'),
-		OverpassMedium: require('../assets/fonts/Overpass-Medium.ttf'),
-		OverpassSemiBold: require('../assets/fonts/Overpass-SemiBold.ttf'),
-		OverpassBold: require('../assets/fonts/Overpass-Bold.ttf'),
-		OverpassExtraBold: require('../assets/fonts/Overpass-ExtraBold.ttf'),
-		OverpassBlack: require('../assets/fonts/Overpass-Black.ttf'),
-	});
+  const [loaded] = useFonts({
+    OverpassExtraLight: require("../assets/fonts/Overpass-ExtraLight.ttf"),
+    OverpassLight: require("../assets/fonts/Overpass-Light.ttf"),
+    Overpass: require("../assets/fonts/Overpass-Regular.ttf"),
+    OverpassMedium: require("../assets/fonts/Overpass-Medium.ttf"),
+    OverpassSemiBold: require("../assets/fonts/Overpass-SemiBold.ttf"),
+    OverpassBold: require("../assets/fonts/Overpass-Bold.ttf"),
+    OverpassExtraBold: require("../assets/fonts/Overpass-ExtraBold.ttf"),
+    OverpassBlack: require("../assets/fonts/Overpass-Black.ttf"),
+  });
 
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
-		}
-	}, [loaded]);
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
-	if (!loaded) {
-		return null;
-	}
+  if (!loaded) {
+    return null;
+  }
 
-	return (
-		<GestureHandlerRootView>
-			<Stack initialRouteName="/" screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="(auth)" options={{ headerShown: false }} />
-				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-				<Stack.Screen name="+not-found" options={{ headerShown: false }} />
-			</Stack>
-		</GestureHandlerRootView>
-	);
+  return (
+    <GestureHandlerRootView>
+      <Stack initialRouteName="/" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+      </Stack>
+      <Toast />
+    </GestureHandlerRootView>
+  );
 }
