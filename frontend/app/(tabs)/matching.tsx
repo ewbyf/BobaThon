22b563585b29311393aee114e3eaa31c56706f61
Aiturgan <aiturgan888@gmail.com>
2024-05-18 01:59:14 -0500

    no pref screen

--- frontend/app/(tabs)/matching.tsx
+++ frontend/app/(tabs)/matching.tsx
@@ -12,18 +12,22 @@ import { useIsFocused } from '@react-navigation/native';
 import api from '@/services/axiosConfig';
 import { getStorage } from '@/lib/storage';
 import Match from '@/components/Match';
+import First from '@/components/icons/First';
+import Second from '@/components/icons/Second';
+import { LinearGradient } from 'expo-linear-gradient';
+import { router } from 'expo-router';
 
 export default function MatchingScreen() {
 	const [bobas, setBobas] = useState<IBoba[]>([]);
 	const activeIndex = useSharedValue(0);
 	const [index, setIndex] = useState(0);
 	const [refreshing, setRefreshing] = useState(false);
-    const [swipe, setSwipe] = useState('0b');
-    const focused = useIsFocused();
-    const [init, setInit] = useState(true);
-    const [hasSetPreferences, setHasSetPreferences] = useState(false);
-    const [preferences, setPreferences] = useState<IPreferences>();
-    const [match, setMatch] = useState(false);
+	const [swipe, setSwipe] = useState('0b');
+	const focused = useIsFocused();
+	const [init, setInit] = useState(true);
+	const [hasSetPreferences, setHasSetPreferences] = useState(false);
+	const [preferences, setPreferences] = useState<IPreferences>();
+	const [match, setMatch] = useState(false);
 
 	useAnimatedReaction(
 		() => activeIndex.value,
@@ -34,116 +38,114 @@ export default function MatchingScreen() {
 		}
 	);
 
-    const onResponse = (res: boolean) => {
+	const onResponse = (res: boolean) => {
 		if (res) {
-            accept();
-        }
+			accept();
+		}
 	};
 
-    useEffect(() => {
-        if (focused) {
-            resetBobaList();
-        }
-
+	useEffect(() => {
+		if (focused) {
+			resetBobaList();
+		}
 	}, [focused]);
 
-    const resetBobaList = () => {
-        const token = getStorage('token');
-        api.get(`/me?token=${token}`)
-        .then((resp) => {
-            setHasSetPreferences(resp.data.hasSetPreferences);
-            setPreferences(resp.data.preferences);
-            api.get(`/matches?token=${token}`)
-            .then((resp2) => {
-                const matches = resp2.data.matches
-                let tempList = [...bobaList.filter((boba) => !matches.includes(boba.id))];
-                shuffle(tempList);
-                setBobas([...tempList]);
-                setIndex(0);
-                setSwipe('0b')
-                activeIndex.value = 0;
-                setInit(false);
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-
-
+	const resetBobaList = () => {
+		const token = getStorage('token');
+		api.get(`/me?token=${token}`)
+			.then((resp) => {
+				setHasSetPreferences(resp.data.hasSetPreferences);
+				setPreferences(resp.data.preferences);
+				api.get(`/matches?token=${token}`)
+					.then((resp2) => {
+						const matches = resp2.data.matches;
+						let tempList = [...bobaList.filter((boba) => !matches.includes(boba.id))];
+						shuffle(tempList);
+						setBobas([...tempList]);
+						setIndex(0);
+						setSwipe('0b');
+						activeIndex.value = 0;
+						setInit(false);
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	const accept = () => {
-        const boba = bobas[Math.floor(activeIndex.value)];
-        if ((boba.tags.includes('Cold') && preferences?.cold) || (boba.tags.includes('Hot') && preferences?.hot)) {
-            if ((preferences?.caffeineFree && boba.tags.includes('Caffeine-free')) || !preferences?.caffeineFree) {
-                if ((preferences?.lactose && boba.tags.includes('Lactose-free')) || !preferences?.lactose) {
-                    let total = 0;
-                    let count = 0
-                    if (preferences?.sweet) {
-                        total++;
-                        if (boba.tags.includes('Sweet')) {
-                            count++;
-                        }
-                    }
-                    if (preferences?.refreshing) {
-                        total++;
-                        if (boba.tags.includes('Refreshing')) {
-                            count++;
-                        }
-                    }
-                    if (preferences?.creamy) {
-                        total++;
-                        if (boba.tags.includes('Creamy')) {
-                            count++;
-                        }
-                    }
-                    if (preferences?.herbal) {
-                        total++;
-                        if (boba.tags.includes('Herbal')) {
-                            count++;
-                        }
-                    }
-                    if (preferences?.fruity) {
-                        total++;
-                        if (boba.tags.includes('Fruity')) {
-                            count++;
-                        }
-                    }
-                    if (preferences?.nutty) {
-                        total++;
-                        if (boba.tags.includes('Nutty')) {
-                            count++;
-                        }
-                    }
-                    if (count / total > .3) { // successful match!
-                        const token = getStorage('token');
-                        api.post('/matches', {
-                            token,
-                            match: bobas[Math.floor(activeIndex.value)].id
-                        })
-                        .then((resp) => {
-                            setMatch(true)
-                        })
-                        .catch((err) => {
-                            console.log(err)
-                        })
-                    }
-                }
-            }
-        }
+		const boba = bobas[Math.floor(activeIndex.value)];
+		if ((boba.tags.includes('Cold') && preferences?.cold) || (boba.tags.includes('Hot') && preferences?.hot)) {
+			if ((preferences?.caffeineFree && boba.tags.includes('Caffeine-free')) || !preferences?.caffeineFree) {
+				if ((preferences?.lactose && boba.tags.includes('Lactose-free')) || !preferences?.lactose) {
+					let total = 0;
+					let count = 0;
+					if (preferences?.sweet) {
+						total++;
+						if (boba.tags.includes('Sweet')) {
+							count++;
+						}
+					}
+					if (preferences?.refreshing) {
+						total++;
+						if (boba.tags.includes('Refreshing')) {
+							count++;
+						}
+					}
+					if (preferences?.creamy) {
+						total++;
+						if (boba.tags.includes('Creamy')) {
+							count++;
+						}
+					}
+					if (preferences?.herbal) {
+						total++;
+						if (boba.tags.includes('Herbal')) {
+							count++;
+						}
+					}
+					if (preferences?.fruity) {
+						total++;
+						if (boba.tags.includes('Fruity')) {
+							count++;
+						}
+					}
+					if (preferences?.nutty) {
+						total++;
+						if (boba.tags.includes('Nutty')) {
+							count++;
+						}
+					}
+					if (count / total > 0.3) {
+						// successful match!
+						const token = getStorage('token');
+						api.post('/matches', {
+							token,
+							match: bobas[Math.floor(activeIndex.value)].id,
+						})
+							.then((resp) => {
+								setMatch(true);
+							})
+							.catch((err) => {
+								console.log(err);
+							});
+					}
+				}
+			}
+		}
 	};
 
-    const onLeft = () => {
-        setSwipe(activeIndex.value.toString() + 'r');
-    }
+	const onLeft = () => {
+		setSwipe(activeIndex.value.toString() + 'r');
+	};
 
-    const onRight = () => {
-        setSwipe(Math.floor(activeIndex.value).toString() + 'a');
-        accept();
-    }
+	const onRight = () => {
+		setSwipe(Math.floor(activeIndex.value).toString() + 'a');
+		accept();
+	};
 
 	const onRefresh = useCallback(() => {
 		setRefreshing(true);
@@ -153,23 +155,64 @@ export default function MatchingScreen() {
 		}, 2000);
 	}, []);
 
-    if (init) {
-        return null;
-    }
+	if (init) {
+		return null;
+	}
 
-    if (!hasSetPreferences) {
-        return null;
-    }
+	if (!hasSetPreferences) {
+		return (
+			<View>
+				<First />
+				<Text style={styles.header}>Set Up Preferences!</Text>
+				<Second />
+				<View style={styles.textCont}>
+					<Text>Get Personalized Recomendations</Text>
+					<Text>and</Text>
+					<Text>Match with the Boba of Your Dreams!</Text>
+				</View>
+				<TouchableOpacity
+					onPress={() =>
+						router.navigate({
+							pathname: '/preferences',
+						})
+					}
+					style={[styles.btn, { marginLeft: 'auto' }]}
+				>
+					<LinearGradient
+						colors={['#EAC5A9', '#E88984']}
+						start={{ x: 0, y: 0 }}
+						end={{ x: 1, y: 0 }}
+						style={{ padding: 2, width: '100%', height: '100%', borderRadius: 15 }}
+					>
+						<LinearGradient
+							colors={['#E98C86', '#E0A694']}
+							start={{ x: 0, y: 0 }}
+							end={{ x: 1, y: 0 }}
+							style={{
+								backgroundColor: 'white',
+								width: '100%',
+								height: '100%',
+								borderRadius: 13,
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+							}}
+						>
+							<Text style={{ fontFamily: 'OverpassBold', fontSize: 16, color: 'white' }}>Start</Text>
+						</LinearGradient>
+					</LinearGradient>
+				</TouchableOpacity>
+			</View>
+		);
+	}
 
-    if (match) {
-        return (
-            <Match img={bobas[Math.floor(activeIndex.value)].img} setMatch={setMatch}></Match>
-        )
-    }
+	if (match) {
+		return <Match img={bobas[Math.floor(activeIndex.value)].img} setMatch={setMatch}></Match>;
+	}
 
 	if (bobas.length == 0 || index == bobas.length) {
 		return (
-			<Container title='Find a Boba!'>
+			<Container title="Find a Boba!">
 				<ScrollView
 					refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
 					showsVerticalScrollIndicator={false}
@@ -183,10 +226,10 @@ export default function MatchingScreen() {
 	}
 
 	return (
-		<Container title='Find a Boba!'>
+		<Container title="Find a Boba!">
 			<View style={{ flex: 1, alignItems: 'center', width: '100%' }}>
 				{bobas.map((boba, i) => (
-					<BobaCard key={boba.id} boba={boba} numOfBobas={bobas.length} index={i} activeIndex={activeIndex} onResponse={onResponse} swipe={swipe}/>
+					<BobaCard key={boba.id} boba={boba} numOfBobas={bobas.length} index={i} activeIndex={activeIndex} onResponse={onResponse} swipe={swipe} />
 				))}
 
 				<TouchableOpacity style={styles.leftButton} onPress={onLeft}>
@@ -204,7 +247,7 @@ const styles = StyleSheet.create({
 	scrollView: {
 		height: '100%',
 		width: '100%',
-		display: 'flex'
+		display: 'flex',
 	},
 	leftButton: {
 		position: 'absolute',
@@ -213,10 +256,10 @@ const styles = StyleSheet.create({
 		zIndex: 999,
 		shadowOffset: {
 			width: 0,
-			height: 2
+			height: 2,
 		},
 		shadowOpacity: 0.25,
-		shadowRadius: 4
+		shadowRadius: 4,
 	},
 	rightButton: {
 		position: 'absolute',
@@ -225,9 +268,21 @@ const styles = StyleSheet.create({
 		zIndex: 999,
 		shadowOffset: {
 			width: 0,
-			height: 2
+			height: 2,
 		},
 		shadowOpacity: 0.25,
-		shadowRadius: 4
-	}
+		shadowRadius: 4,
+	},
+	// NO PREF SET
+	header: {},
+	textCont: {},
+	btn: {
+		width: 100,
+		height: 40,
+		backgroundColor: 'white',
+		borderRadius: 15,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
